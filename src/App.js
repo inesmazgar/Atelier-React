15ec6195f8098fcce549b3b322409522fe724626
Ines Mazgar <162985091+inesmazgar@users.noreply.github.com>
2024-10-21 11:20:14 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Products from './Component/Products'
+import Welcome from './Component/Welcome';
+function App() {
+  return (
+    <div className="App">
+      <Welcome></Welcome>
+     <Products></Products>
+     
+    </div>
+  );
+}
+
+export default App;
